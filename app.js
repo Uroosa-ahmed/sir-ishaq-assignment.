6e0d20b65321e7bff4415bb18bd64db60b6d4c12
Uroosa ahmed <uroosaahmed555@gmail.com>
2023-04-07 19:44:59 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+// let userfavNum =prompt("Enter your favourite number");
+// console.log(+userfavNum + 5)
+
+// comparison operator
+// ==,===
+// console.log(5 === "6");
+
+// Task 1
+// let num1 = 7;
+// let num2 = 6;
+// console.log(num1 > num2);
+
+// task 2
+// let userfavNum =prompt("Enter your number");
+// if(userfavNum>0)
+// {alert("positive +")}
+
+//  else if (userfavNum<0)
+//  {alert("negative -")}
+
+// console.log(userfavNum);
+
+// Task 3
+// let num1 = 17;
+// let num2 = 6;
+// let num3 = 8;
+// let num4 = 15;
+// let num5 = 3
+// if (num1>num2 && num1>num3 && num1>num4 && num1>num5)
+// {console.log(num1);}
+
+// Task 4
+
+// var x = prompt( "enter your number");
+// if(x%2==0)
+// {console.log("even number");
+// }
+// else
+// {console.log("odd number");
+// }
+
+// 
+
+// 
+
+
+// Task 6
+
+// for (let num= 1; num <=100; num++)
+// {
+//     if (num % 3 ===0 && num % 5 ===0){
+
+//         console.log("fizzbuz");
+//     }
+// else if (num % 3 ===0){
+//     console.log("fizz");
+// }
+
+// else if (num % 5 ===0)
+
+// console.log("buzz");
+// {
+//     console.log(num);
+// }
+
+// }
+// Task 7
+
+// for(i = 1; i<=5; i++){
+
+// for(j= 1;j<=i; j++){
+//   console.log("*");
+//   document.write("*")
+
+// }
+// document.write("<br>");
+
+// }
+
+
+
+
+
+
+
+
+
